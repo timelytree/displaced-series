c87c8db6fb47f93ad0cc654218f985490238dd09
Nauras Jabari <nauras@moogmedia.ca>
2017-10-01 08:43:07 -0700

    Updated API endpoints to new website

--- src/store/methods.js
+++ src/store/methods.js
@@ -10,42 +10,42 @@ export default {
   addC: (item, className) => { item.classList.add(className) },
   remC: (item, className) => { item.classList.remove(className) },
   gA: (item, att) => { return item.getAttribute('data-' + att + '') },
-  // ------------------------------------------- Use Moment.js to formate dates
+  // -------------------------------------------- Use Moment.js to format dates
   // --------------------------------------------------------------------------
   formatDate: moment,
   // --------------------------------------------------------------- Fetch page
   // --------------------------------------------------------------------------
   fetchSinglePage: function (cb, pageId) {
     axios
-      .get('https://displaced.000webhostapp.com/wp-json/wp/v2/pages/' + pageId + '')
+      .get('http://67.207.85.161/displaced/wp-json/wp/v2/pages/' + pageId + '')
       .then(response => { cb(response.data) })
   },
   // ---------------------------------------------------- Fetch home page posts
   // --------------------------------------------------------------------------
   fetchAllPosts: function (cb) {
     axios
-      .get('https://displaced.000webhostapp.com/wp-json/wp/v2/posts')
+      .get('http://67.207.85.161/displaced/wp-json/wp/v2/posts')
       .then(response => { cb(response.data) })
   },
   // -------------------------------------------------------- Fetch single post
   // --------------------------------------------------------------------------
   fetchSinglePost: function (cb, postId) {
     axios
-      .get('https://displaced.000webhostapp.com/wp-json/wp/v2/posts/' + postId + '')
+      .get('http://67.207.85.161/displaced/wp-json/wp/v2/posts/' + postId + '')
       .then(response => { cb(response.data) })
   },
   // ------------------------------------------------------- Fetch recent posts
   // --------------------------------------------------------------------------
   fetchRecentPosts: function (cb) {
     axios
-      .get('https://displaced.000webhostapp.com/wp-json/wp/v2/posts?per_page=3')
+      .get('http://67.207.85.161/displaced/wp-json/wp/v2/posts?per_page=3')
       .then(response => { cb(response.data) })
   },
   // -------------------------------------- Fetch gif for menu background image
   // --------------------------------------------------------------------------
   fetchMenuOverlayBackgroundImg: function (cb) {
     axios
-      .get('https://displaced.000webhostapp.com/wp-json/wp/v2/media/75')
+      .get('http://67.207.85.161/displaced/wp-json/wp/v2/media/75')
       .then(response => { cb(response.data) })
   },
   // ---------------------------------------------------------- Menu Open/Close
