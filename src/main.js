e2bd23c1e27bda850adbe4668a96e89908740bdb
Nauras Jabari <nauras@moogmedia.ca>
2017-10-11 12:18:53 -0700

    Implemented Analytics tracking

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAnalytics from 'vue-analytics'
 import router from './router'
 import '../node_modules/bootstrap/scss/bootstrap.scss'
 import './stylesheets/main.scss'
 
 Vue.config.productionTip = false
+if (window.location.hostname !== 'localhost') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-92177503-4',
+    router
+  })
+}
 
 /* eslint-disable no-new */
 new Vue({
